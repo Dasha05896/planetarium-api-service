bb758cde61f71e45063361b2f85a0005586d5fd2
Daria Budovich <Budovich_Dasha@ukr.net>
2026-02-15 20:52:43 +0200

    feat: implement user registration and authentication endpoints

--- user/urls.py
+++ user/urls.py
@@ -4,16 +4,14 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView,
 )
-
-# Тут ми пізніше додамо твій CreateUserView
-# from user.views import CreateUserView, ManageUserView
+from user.views import CreateUserView, ManageUserView
 
 urlpatterns = [
-    # path("register/", CreateUserView.as_view(), name="create"),
+    path("register/", CreateUserView.as_view(), name="create"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
-    # path("me/", ManageUserView.as_view(), name="manage"),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
 
 app_name = "user"
